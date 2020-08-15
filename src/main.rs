ede558cd31ad743fc75ea87600ed13e5fc4967dc
Rail Khamitov <mshassium@gmail.com>
2020-08-15 23:48:31 +0400

    first example

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+
+
+use futures::StreamExt;
+use telegram_bot::*;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let token = "1218027891:AAFXqS_nfC-hRp7WqQCnWMpYPyouZKiONhA";
+    let api = Api::new(token);
+
+    // Fetch new updates via long poll method
+    let mut stream = api.stream();
+    while let Some(update) = stream.next().await {
+        // If the received update contains a new message...
+        let update = update?;
+        if let UpdateKind::Message(message) = update.kind {
+            if let MessageKind::Text { ref data, .. } = message.kind {
+                // Print received text message to stdout.
+                println!("<{}>: {}", &message.from.first_name, data);
+
+                // Answer message with "Hi".
+                api.send(message.text_reply(format!(
+                    "Hi, {}! You just wrote '{}'",
+                    &message.from.first_name, data
+                )))
+                    .await?;
+            }
+        }
+    }
+    Ok(())
+}
